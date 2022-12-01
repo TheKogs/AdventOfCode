3d231d3b491fce43758693fdf64772939c598bcf
TheKogs <kogs@kogs.info>
2022-12-01 10:32:54 +0100

    Challenge for Day01

---
+++ AdventOfCode/Day01/Day01.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Day01;
+
+public static class Day01
+{
+    public static void Execute(string input)
+    {
+        var sortedCaloriesList = input
+            .Trim()
+            .Split($"{Environment.NewLine}{Environment.NewLine}") 
+            .Select(elvesCalories =>
+                elvesCalories.Split(Environment.NewLine)
+                    .Sum(int.Parse))
+            .OrderByDescending(x => x)
+            .ToList();
+        
+        Console.WriteLine("Day01");
+        Console.WriteLine($"\tTop elv calories = {sortedCaloriesList.First()}");
+        Console.WriteLine($"\tTop 3 elves calories = {sortedCaloriesList.Take(3).Sum()}");
+    }
+  
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode.Day01;
+
+Console.WriteLine("Hello, AdventOfCode");
+
+Day01.Execute(File.ReadAllText("Day01/Input.txt"));
