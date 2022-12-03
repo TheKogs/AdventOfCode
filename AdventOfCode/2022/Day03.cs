e756a003fc9d99ed97c2b55ca35a397b1d303403
TheKogs <kogs@kogs.info>
2022-12-03 07:37:55 +0100

    Added Day03 challenge

---
+++ AdventOfCode/2022/Day03.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode._2022;
+
+public static class Day03
+{
+	public static void Execute(string input)
+	{
+		var commonItemsPrioritySum = input
+			.Trim()
+			.Split(Environment.NewLine)
+			.Select(line =>
+				GetItemValue(
+					line[..(line.Length / 2)]
+						.Intersect(line[(line.Length / 2)..])
+						.First()))
+			.Sum();
+
+		var index = 0;
+		var stickersPrioritySum = input
+			.Trim()
+			.Split(Environment.NewLine)
+			.GroupBy(x => index++ / 3)
+			.Select(group =>
+				GetItemValue(group
+					.Aggregate<IEnumerable<char>>((previous, next) => 
+						previous.Intersect(next))
+					.First()))
+			.Sum();
+		
+		Console.WriteLine("Day03");
+		Console.WriteLine($"\tPriority Sum Part 1: {commonItemsPrioritySum}");
+		Console.WriteLine($"\tPriority Sum Part 2: {stickersPrioritySum}");
+	}
+
+	private static int GetItemValue(char item) =>
+		item switch
+		{
+			>= 'a' and <= 'z' => item - 'a' + 1,
+			>= 'A' and <= 'Z' => item - 'A' + 26 + 1,
+			_ => 0
+		};
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,3 +5,12 @@ using AdventOfCode._2022;
 Console.WriteLine("Hello, AdventOfCode");
 
 Day01.Execute(File.ReadAllText("2022/Day01_Input.txt"));
+Day02.Execute(File.ReadAllText("2022/Day02_Input.txt"));
+var x = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+// Day03.Execute(x);
+Day03.Execute(File.ReadAllText("2022/Day03_Input.txt"));
