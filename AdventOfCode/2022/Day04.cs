9a976cd69befa2f172215f78c3da952d226b6002
TheKogs <kogs@kogs.info>
2022-12-04 07:33:17 +0100

    Refactoring of common code

--- AdventOfCode/2022/Day04.cs
+++ AdventOfCode/2022/Day04.cs
@@ -4,20 +4,19 @@ public static class Day04
 {
 	public static void Execute(string input)
 	{
-		var fullOverlap = input
+		var sections = input
 			.Trim()
 			.Split(Environment.NewLine)
 			.Select(line => line.Split(',', '-').Select(int.Parse).ToList())
-			.Select(s => (Enumerable.Range(s[0], s[1]-s[0]+1), Enumerable.Range(s[2], s[3]-s[2]+1)))
+			.Select(s => (Enumerable.Range(s[0], s[1] - s[0] + 1), Enumerable.Range(s[2], s[3] - s[2] + 1)))
+			.ToList();
+
+		var fullOverlap = sections 
 			.Select(s => (s.Item1.Intersect(s.Item2).Count(), s.Item1.Count(), s.Item2.Count()))
 			.Select(s => s.Item1 == s.Item2 || s.Item1 == s.Item3 ? 1 : 0)
 			.Sum();
 		
-		var partialOverlap = input
-			.Trim()
-			.Split(Environment.NewLine)
-			.Select(line => line.Split(',', '-').Select(int.Parse).ToList())
-			.Select(s => (Enumerable.Range(s[0], s[1]-s[0]+1), Enumerable.Range(s[2], s[3]-s[2]+1)))
+		var partialOverlap = sections
 			.Select(s=> s.Item1.Intersect(s.Item2).Any() ? 1 : 0)
 			.Sum();
 		
