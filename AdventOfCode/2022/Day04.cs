bf8cac44118186e77343d58c22aac28049b6790c
TheKogs <kogs@kogs.info>
2022-12-04 07:25:13 +0100

    Added Day 4 challenge

---
+++ AdventOfCode/2022/Day04.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode._2022;
+
+public static class Day04
+{
+	public static void Execute(string input)
+	{
+		var fullOverlap = input
+			.Trim()
+			.Split(Environment.NewLine)
+			.Select(line => line.Split(',', '-').Select(int.Parse).ToList())
+			.Select(s => (Enumerable.Range(s[0], s[1]-s[0]+1), Enumerable.Range(s[2], s[3]-s[2]+1)))
+			.Select(s => (s.Item1.Intersect(s.Item2).Count(), s.Item1.Count(), s.Item2.Count()))
+			.Select(s => s.Item1 == s.Item2 || s.Item1 == s.Item3 ? 1 : 0)
+			.Sum();
+		
+		var partialOverlap = input
+			.Trim()
+			.Split(Environment.NewLine)
+			.Select(line => line.Split(',', '-').Select(int.Parse).ToList())
+			.Select(s => (Enumerable.Range(s[0], s[1]-s[0]+1), Enumerable.Range(s[2], s[3]-s[2]+1)))
+			.Select(s=> s.Item1.Intersect(s.Item2).Any() ? 1 : 0)
+			.Sum();
+		
+		Console.WriteLine("Day04");
+		Console.WriteLine($"\tFull overlap sections: {fullOverlap}");
+		Console.WriteLine($"\tPartial overlap sections: {partialOverlap}");
+	}
+
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -7,3 +7,4 @@ Console.WriteLine("Hello, AdventOfCode");
 Day01.Execute(File.ReadAllText("2022/Day01_Input.txt"));
 Day02.Execute(File.ReadAllText("2022/Day02_Input.txt"));
 Day03.Execute(File.ReadAllText("2022/Day03_Input.txt"));
+Day04.Execute(File.ReadAllText("2022/Day04_Input.txt"));
