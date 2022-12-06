537e71905a461c4e7b4e42c39422381639f866b3
TheKogs <kogs@kogs.info>
2022-12-06 07:28:53 +0100

    Day04 challenge - small readability improvements

--- AdventOfCode/2022/Day04.cs
+++ AdventOfCode/2022/Day04.cs
@@ -4,21 +4,21 @@ public static class Day04
 {
 	public static void Execute(string input)
 	{
-		var sections = input
+		var sectionsInput = input
 			.Trim()
 			.Split(Environment.NewLine)
 			.Select(line => line.Split(',', '-').Select(int.Parse).ToList())
-			.Select(s => (Elv1Sections: Enumerable.Range(s[0], s[1] - s[0] + 1), Elv2Sections: Enumerable.Range(s[2], s[3] - s[2] + 1)))
+			.Select(s => (Elv1: Enumerable.Range(s[0], s[1] - s[0] + 1), Elv2: Enumerable.Range(s[2], s[3] - s[2] + 1)))
 			.ToList();
 
-		var fullOverlap = sections 
-			.Select(s => (IntersectCount: s.Elv1Sections.Intersect(s.Elv2Sections).Count(), Elv1Count: s.Item1.Count(), Elv2Count: s.Item2.Count()))
-			.Select(s => s.IntersectCount == Math.Min(s.Elv1Count, s.Elv2Count) ? 1 : 0)
-			.Sum();
-		
-		var partialOverlap = sections
-			.Select(s=> s.Elv1Sections.Intersect(s.Elv2Sections).Any() ? 1 : 0)
-			.Sum();
+		var fullOverlap = sectionsInput
+			.Select(sections => (
+				IntersectCount: sections.Elv1.Intersect(sections.Elv2).Count(), 
+				SmallerSectionCount: Math.Min(sections.Item1.Count(), sections.Item2.Count())))
+			.Count(s => s.IntersectCount == s.SmallerSectionCount);
+
+		var partialOverlap = sectionsInput
+			.Count(sections => sections.Elv1.Intersect(sections.Elv2).Any());
 		
 		Console.WriteLine("Day04");
 		Console.WriteLine($"\tFull overlap sections: {fullOverlap}");
