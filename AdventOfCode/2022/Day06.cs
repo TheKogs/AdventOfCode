dcbb9b00cb92118654aa7fabe2fb0a53812c7b92
TheKogs <kogs@kogs.info>
2022-12-06 13:23:46 +0100

    Day 6 challenge added

---
+++ AdventOfCode/2022/Day06.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode._2022;
+
+public static class Day06
+{
+	public static void Execute(string input)
+	{
+		var sequence = input.ToCharArray();
+
+		var startOfPacket = CountElementsTillMarker(sequence, 4);
+		var startOfMessage = CountElementsTillMarker(sequence, 14);
+		
+		Console.WriteLine("Day06");
+		Console.WriteLine($"\tStart of Packet Index: {startOfPacket}");
+		Console.WriteLine($"\tStart of Message Index: {startOfMessage}");
+	}
+
+	private static int CountElementsTillMarker(IReadOnlyCollection<char> sequence, int markerSize) =>
+		( from i in Enumerable.Range(0, sequence.Count - markerSize)
+		   let cntDuplicates = sequence
+				   .Skip(i)
+				   .Take(markerSize)
+				   .GroupBy(x => x)
+				   .Count(g => g.Count() > 1)
+		 where cntDuplicates == 0
+		select i + markerSize)
+		.FirstOrDefault();
+	
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,3 +9,4 @@ Day02.Execute(File.ReadAllText("2022/Day02_Input.txt"));
 Day03.Execute(File.ReadAllText("2022/Day03_Input.txt"));
 Day04.Execute(File.ReadAllText("2022/Day04_Input.txt"));
 Day05.Execute(File.ReadAllText("2022/Day05_Input.txt"));
+Day06.Execute(File.ReadAllText("2022/Day06_Input.txt"));
